bc48c62a683b2bb3fc3f03f8b6d90958105a8ed4
Anja Rankovic <149586707+AnjaRankovic@users.noreply.github.com>
2026-06-21 22:27:48 +0200

    Update DN10.cpp

--- Domace naloge/DN10.cpp
+++ Domace naloge/DN10.cpp
@@ -1,68 +1,53 @@
 // #include <Arduino.h>
 // #include <WiFi.h>
-// #include <WebServer.h>
-// #include <WebSocketsServer.h>
+// #include <WebSocketsClient.h>
+// #include <ArduinoJson.h>
 
 // const char *ssid = "samoanja";
 // const char *password = "samoanja25";
 
-// WebServer server(80);
-// WebSocketsServer webSocket = WebSocketsServer(81);
+// int portNumber = 8811;
+// WebSocketsClient webSocket;
 
-// const char HTML[] PROGMEM = R"rawliteral(
-// <!DOCTYPE html>
-// <html lang="sl">
-// <head>
-//     <meta charset="UTF-8">
-//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-//     <title>WebSocket</title>
-// </head>
-// <body>
-// <h2>Primer s spletnim vtičnikom WebSocket</h2>
-// <input type="text" id="sporočilo" placeholder="Vnesite sporočilo">
-// <button onclick="pošljiSporočilo()">pošljiSporočilo</button>
-// <script>
-// let povezava = new WebSocket("ws://10.252.254.63:81/");
-// function pošljiSporočilo(){
-//     let sporočilo = document.getElementById("sporočilo").value;
-//     povezava.send(sporočilo);
-// }
-// </script>
-// </body>
-// </html>
-// )rawliteral";
+// String dataString;
+// const int nozicaPotenciometra = 34;
+// int vrednostPotenciometra = 0;
+// int prethodnaVrednost = 0;
 
-// void handle_root()
-// {
-//     server.send(200, "text/html", HTML);
-// }
-
-// void onWebSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
+// void onWebSocketEvent(WStype_t type, uint8_t *payload, size_t length)
 // {
 //     switch (type)
 //     {
-//     case WStype_DISCONNECTED:
-//     {
-//         Serial.printf("[%u] Klient odklopljen!\n", num);
-//     }
-//     break;
-
-//     case WStype_CONNECTED:
-//     {
-//         IPAddress ip = webSocket.remoteIP(num);
-//         Serial.printf("[%u] Vzpostavljena WebSocket povezava iz IP naslova!\n", num);
-//         Serial.println(ip.toString());
-//     }
-//     break;
-
 //     case WStype_TEXT:
-//     {
-//         Serial.printf("[%u] Besedilo sporočila: %s\n", num, payload);
-//     }
-//     break;
+//         JsonDocument doc;
+//         DeserializationError error = deserializeJson(doc, payload);
 
-//     default:
-//         break;
+//         if (error)
+//         {
+//             Serial.print(F("deserializeJson() failed: "));
+//             Serial.println(error.c_str());
+//             return;
+//         }
+
+//         const char *tip = doc["tipPoruke"];
+
+//         if (tip != nullptr && strcmp(tip, "LED") == 0)
+//         {
+//             int pin = doc["pin"];
+//             int vrednost = doc["vrednost"];
+
+//             if (pin == 2)
+//             {
+//                 digitalWrite(2, vrednost == 1 ? HIGH : LOW);
+//                 Serial.print("LED na pin 2 ");
+//                 Serial.println(vrednost == 1 ? "UKLJUCEN" : "ISKLJUCEN");
+//             }
+//             else
+//             {
+//                 Serial.println("Neprepoznata vrednost u JSON sporocilu!");
+//             }
+//             break;
+//         }
 //     }
 // }
 
@@ -70,38 +55,48 @@
 // {
 //     Serial.begin(115200);
 
+//     Serial.println("Povezovanje na WiFi omrezje...");
 //     WiFi.begin(ssid, password);
-
 //     while (WiFi.status() != WL_CONNECTED)
 //     {
 //         delay(500);
-//         Serial.println("Povezovanje na WiFi omrežje v teku...");
+//         Serial.print(".");
 //     }
-
-//     Serial.println("Povezava z WiFi omrežjem je vzpostavljena.");
-//     Serial.print("IP naslov modula = ");
+//     Serial.println();
+//     Serial.println("Povezano na WiFi omrezje!");
+//     Serial.print("IP naslov naprave: ");
 //     Serial.println(WiFi.localIP());
 
-//     server.on("/", handle_root);
-//     server.begin();
-//     Serial.println("HTTP strežnik je zagnan, vpišite IP naslov v brskalnik, npr. http://10.252.254.63/ http in ne https.");
-
-//     webSocket.begin();
-//     webSocket.onEvent(onWebSocketEvent);
-
 //     pinMode(2, OUTPUT);
-//     digitalWrite(2, HIGH);
-//     delay(750);
-//     digitalWrite(2, LOW);
-//     delay(750);
-//     digitalWrite(2, HIGH);
-//     delay(750);
-//     digitalWrite(2, LOW);
-//     delay(750);
+//     digitalWrite(2, 1);
+//     delay(200);
+//     digitalWrite(2, 0);
+//     delay(200);
+
+//     // Popravljeno na tvoju novu IP adresu i port 8811
+//     webSocket.begin("10.252.254.48", 8811);
+//     delay(1000);
+//     webSocket.onEvent(onWebSocketEvent);
 // }
 
 // void loop()
 // {
-//     server.handleClient();
 //     webSocket.loop();
+
+//     vrednostPotenciometra = analogRead(nozicaPotenciometra);
+
+//     // Salje podatak samo ako se vrednost promenila za vise od 15 crtica
+//     if (abs(vrednostPotenciometra - prethodnaVrednost) > 15)
+//     {
+//         Serial.println(vrednostPotenciometra);
+
+//         // Promenjen kljuc u tipPoruke da se poklapa sa Node.js i HTML-om
+//         dataString = R"({"tipPoruke":"potenciometar","pin":34,"vrednost":)";
+//         dataString = dataString + String(vrednostPotenciometra) + "}";
+
+//         webSocket.sendTXT(dataString);
+//         prethodnaVrednost = vrednostPotenciometra;
+//     }
+
+//     delay(30);
 // }
