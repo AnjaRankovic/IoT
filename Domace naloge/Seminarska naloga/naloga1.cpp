1476cf7f0bd36719a368d5d21e85537320cb867f
Anja Rankovic <149586707+AnjaRankovic@users.noreply.github.com>
2026-06-21 23:01:43 +0200

    Update naloga1.cpp

--- Domace naloge/Seminarska naloga/naloga1.cpp
+++ Domace naloge/Seminarska naloga/naloga1.cpp
@@ -6,7 +6,7 @@
 // // --- TVOJI PODACI ZA WIFI I SERVER ---
 // const char *ssid = "samoanja";
 // const char *password = "samoanja25";
-// const char *serverIP = "192.168.1.205.48"; // IP adresa tvog Windows servera (.48)
+// const char *serverIP = "192.168.1.205"; // IP adresa tvog Windows servera 
 
 // // --- TVOJ TOČAN RASPORED PINOVA SA PLOČICE ---
 // const int pinGlavnaLED = 5;    // Žuta LED
